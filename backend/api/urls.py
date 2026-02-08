cb42ade0a16a3bbccbeba55081df325109ab50fe
PRIYANSU DASH <100258436+shavedXpixel@users.noreply.github.com>
2026-02-08 16:53:34 +0530

    changing backend

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('exam/<uuid:exam_id>/', views.get_exam),
-    path('submit/<uuid:exam_id>/', views.submit_exam),
-    path('check/<uuid:exam_id>/', views.check_status),
-]
